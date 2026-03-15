0cdf22935e14077f16420a33513be2de32bb42d6
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-16 00:35:57 +0100

    Update storage.go

--- Backend/internal/storage/storage.go
+++ Backend/internal/storage/storage.go
@@ -6,24 +6,34 @@ import (
 	"time"
 )
 
+const (
+	ERROR_NO_ROWS_AFFECTED = "no rows affected"
+)
+
 type Storage struct {
 	UserStorage interface {
 		GetById(context.Context, int64) (*User, error)
+		RegisterUser(ctx context.Context, user *User) error
 	}
 	StatsStorage interface {
 		GetUsersLast(context.Context, int64) (*UsageRecord, error)
 		AddNewRecord(context.Context, int64, int32, time.Time) error
 		GetGroupStats(context.Context, string) ([]*GroupStats, error)
 	}
-	GroupsStorage interface {
+	GroupStorage interface {
 		CheckIfMember(context.Context, int64, string) bool
+		CreateGroup(context.Context, string) (string, error)
+		JoinGroup(ctx context.Context, userId int64, inviteCode string) error
+		LeaveGroup(ctx context.Context, userId int64, groupId string) error
 	}
 }
 
 func NewStorage(db *sql.DB) *Storage {
 	return &Storage{
-		UserStorage:   &UserStorage{db},
-		StatsStorage:  &StatsStorage{db},
-		GroupsStorage: &GroupsStorage{db},
+		UserStorage:  &UserStorage{db},
+		StatsStorage: &StatsStorage{db},
+		GroupStorage: &GroupStorage{db},
 	}
 }
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
