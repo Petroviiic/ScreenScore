cb29f392d8919fab55f746cc8f308afefffec60c
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-17 00:17:04 +0100

    Update health.go

--- Backend/cmd/api/health.go
+++ Backend/cmd/api/health.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+// CreateGroup godoc
+// @Summary      Health check
+// @Description  Checks if the server is active.
+// @Tags         utils
+// @Success      200
+// @Router       /health [get]
 func (app *Application) GetHealth(w http.ResponseWriter, r *http.Request) {
 	if err := jsonResponse(w, http.StatusOK, "ok"); err != nil {
 		log.Panic("something went wrong")
