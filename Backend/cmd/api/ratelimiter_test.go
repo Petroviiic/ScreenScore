43b0e60e22a3ab902ab36cbd838e02380d430028
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-19 01:28:11 +0100

    added fixedwindow rate limiter

---
+++ Backend/cmd/api/ratelimiter_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestRatelimiter(t *testing.T) {
+	app := newTestApplication(t)
+	mux := app.mount()
+	t.Run("rate limit test", func(t *testing.T) {
+		for range 200 {
+			req, err := http.NewRequest(http.MethodGet, "/v1/health", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			resp := executeRequest(req, mux)
+
+			checkResponseCode(t, http.StatusTooManyRequests, resp.Code)
+		}
+	})
+}
