ef83d87ecaee5fd19ed8e4a9a23ba82231521b28
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-26 22:50:09 +0100

    add preset messages table

---
+++ Backend/cmd/migrate/migrations/000009_add_preset_messages_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS preset_messages(
+    id BIGSERIAL PRIMARY KEY,
+    message TEXT NOT NULL DEFAULT 'message',
+    price INT NOT NULL DEFAULT 1000,
+    rarity VARCHAR(20) NOT NULL DEFAULT 'common',
+    is_active BOOLEAN DEFAULT true,       
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
