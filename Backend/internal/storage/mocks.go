1dbc7f8b058ee106cff6c8a2ee48d24a6c06961d
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-04-10 23:22:20 +0200

    Update mocks.go

--- Backend/internal/storage/mocks.go
+++ Backend/internal/storage/mocks.go
@@ -7,13 +7,16 @@ import (
 
 func NewMockStorage() *Storage {
 	return &Storage{
-		StatsStorage:  &StatsMockStorage{},
-		DeviceStorage: &DeviceMockStorage{},
+		StatsStorage:      &StatsMockStorage{},
+		DeviceStorage:     &DeviceMockStorage{},
+		UserStreakStorage: &UserStreakMockStorage{},
 	}
 }
 
 type StatsMockStorage struct {
-	GetUsersLastFunc func(ctx context.Context, userID int64) (*UsageRecord, error)
+	GetUsersLastFunc                    func(ctx context.Context, userID int64) (*UsageRecord, error)
+	GetUserScreenTimeForDayFunc         func(ctx context.Context, userID int64) (int, error)
+	GetUserAverageScreenTimeForWeekFunc func(ctx context.Context, userID int64) (float64, error)
 }
 
 func (s *StatsMockStorage) GetUsersLast(context.Context, int64, string) (*UsageRecord, error) {
@@ -24,6 +27,13 @@ func (s *StatsMockStorage) AddNewRecord(context.Context, int64, int32, string, t
 }
 func (s *StatsMockStorage) GetGroupStats(context.Context, string, time.Time) ([]*GroupStats, error) {
 	return nil, nil
+}
+
+func (s *StatsMockStorage) GetUserAverageScreenTimeForWeek(context.Context, time.Time, time.Time) (float64, error) {
+	return s.GetUserAverageScreenTimeForWeekFunc(nil, 0)
+}
+func (s *StatsMockStorage) GetUserScreenTimeForDay(context.Context, time.Time) (int, error) {
+	return s.GetUserScreenTimeForDayFunc(nil, 0)
 }
 
 type DeviceMockStorage struct {
@@ -42,3 +52,16 @@ func (d *DeviceMockStorage) UpdateLastSeen(ctx context.Context, userId int64, de
 	return nil
 }
 func (d *DeviceMockStorage) DeleteFCMToken(string) error { return nil }
+
+type UserStreakMockStorage struct {
+	GetStreakDataFunc func(ctx context.Context, userID int64) (*StreakData, error)
+}
+
+func (s *UserStreakMockStorage) GetStreakData(ctx context.Context, userID int64) (*StreakData, error) {
+	return s.GetStreakDataFunc(nil, 0)
+}
+
+func (s *UserStreakMockStorage) SaveStreak(ctx context.Context, userID int64, data *StreakData) error {
+
+	return nil
+}
